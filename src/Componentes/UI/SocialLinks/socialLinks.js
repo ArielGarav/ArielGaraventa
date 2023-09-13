99a0c6bf7c50b0580cc16d0e9c240d5ebbf08b20
ArielGarav <arielgaraventa5@gmail.com>
2023-09-13 14:19:47 -0300

    Actualizacion de Links Cv

--- src/Componentes/UI/SocialLinks/socialLinks.js
+++ src/Componentes/UI/SocialLinks/socialLinks.js
@@ -1,5 +1,5 @@
 export const socialLinks = {
-  Cv: "https://drive.google.com/file/d/1Kflh_iNKnvFg1xoPLn1Pi2KVJGbB18Xe/view?usp=sharing",
+  Cv: "https://drive.google.com/drive/folders/1cAhsIok5lfZT7NGgeeL3z6WtgEUWK3uh?usp=sharing",
   Linkedin: "https://www.linkedin.com/in/ariel-garaventa",
   Github: "https://github.com/ArielGarav",
   Twitter: "https://twitter.com/ariel_gara86737",
